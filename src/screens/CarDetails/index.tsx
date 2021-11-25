3ba70e802362bb1f42737e9c2d8c5ef2f9d05525
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-11-25 19:53:25 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import {
  Container,
  Header,
+ CarImages,
 } from './styles';
 
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 export function CarDetails(){
    return (
     <Container>
         <Header>
             <BackButton onPress={() => {}} />
-
         </Header>
+
+        <CarImages>
+            <ImageSlider 
+                imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']} 
+            />
+        </CarImages>
     </Container>
    );
 }
