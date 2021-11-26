826db1588489f4f370858e8e0ce3783c87de68a8
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-11-26 17:44:05 -0300

    COmeço da interface de agendamento

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
 
 import { Home } from './src/screens/Home';
 import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
 
 import theme from './src/styles/theme';
 
@@ -35,7 +36,7 @@ export default function App() {
     <ThemeProvider
       theme={theme}
     >
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
