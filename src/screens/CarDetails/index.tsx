d032605314917f70d78d7b466c771415949e8be3
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-11-26 13:12:49 -0300

    Continuando a Interface de detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,6 +3,15 @@ import {
  Container,
  Header,
  CarImages,
+ Content,
+ Details,
+ Description,
+ Brand,
+ Name,
+ Rent,
+ Period,
+ Price,
+ About
 } from './styles';
 
 import { BackButton } from '../../components/BackButton';
@@ -20,6 +29,25 @@ export function CarDetails(){
                 imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']} 
             />
         </CarImages>
+
+        <Content>
+            <Details>
+                <Description>
+                    <Brand>Lamborghini</Brand>
+                    <Name>Huracan</Name>
+                </Description>
+
+                <Rent>
+                    <Period>Ao dia</Period>
+                    <Price>R$ 580</Price>
+                </Rent>
+            </Details>
+
+            <About>
+                Este é um autómovel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+            </About>
+        </Content>
+
     </Container>
    );
 }
