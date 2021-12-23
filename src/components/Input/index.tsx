937d3037e21b8ec52ebdba6405c53069112482aa
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-12-23 13:16:02 -0300

    Input de Email

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,22 +4,30 @@ import { useTheme } from 'styled-components';
 import { Feather } from '@expo/vector-icons';
 
 import {
- Container
+ Container,
+ IconContainer,
+ InputText,
 } from './styles';
 
 interface Props extends TextInputProps {
     iconName: React.ComponentProps<typeof Feather>['name'];
 }
 
-export function Input({ iconName }: Props){
+export function Input({ iconName, ...rest }: Props){
     const theme = useTheme();
 
    return (
     <Container>
-        <Feather
-            name={iconName}
-            size={24}
-            color={theme.colors.text_detail}
+        <IconContainer>
+            <Feather
+                name={iconName}
+                size={24}
+                color={theme.colors.text_detail}
+            />
+        </IconContainer>
+
+        <InputText 
+            {...rest}
         />
 
     </Container>
