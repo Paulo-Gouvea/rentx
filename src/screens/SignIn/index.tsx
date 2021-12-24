2149d7533065e219e4a9c7ba555442de8a0ce942
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-12-24 14:16:07 -0300

    Validação do Formulario

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,8 +3,10 @@ import {
     StatusBar,
     KeyboardAvoidingView, 
     TouchableWithoutFeedback,
-    Keyboard, 
+    Keyboard,
+    Alert, 
 } from 'react-native';
+import * as Yup from 'yup';
 
 import {
  Container,
@@ -26,6 +28,30 @@ export function SignIn(){
     const [password, setPassword] = useState('');
 
    const theme = useTheme();
+
+   async function handleSignIn(){
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string()
+                 .required('E-mail obrigatório')
+                 .email('Digite um e-mail válido'),
+                password: Yup.string()
+                 .required('A senha é obrigatória')
+            });
+            
+            await schema.validate({ email, password });
+            Alert.alert('Tudo certo!');
+        } catch (error) {
+            if(error instanceof Yup.ValidationError){
+                Alert.alert('Opa!', error.message);
+            }else{
+                Alert.alert(
+                    'Erro na autenticação',
+                    'Ocorreu um erro ao fazer login, verifique às credenciais'
+                )
+            }
+        }
+   }
    
     return (
         <KeyboardAvoidingView behavior='position' enabled >
@@ -68,8 +94,8 @@ export function SignIn(){
                     <Footer>
                         <Button 
                             title='Login'
-                            onPress={() => {}}
-                            enabled={false}
+                            onPress={handleSignIn}
+                            enabled={true}
                             loading={false}
                         />
                         <Button 
