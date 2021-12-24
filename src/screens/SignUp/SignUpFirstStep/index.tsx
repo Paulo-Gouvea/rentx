dc6e0b14b334d9e3eb6d82b6d90db7309fc21361
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-12-24 15:47:46 -0300

    Primeiro Step

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+   KeyboardAvoidingView,
+   TouchableWithoutFeedback,
+   Keyboard,
+} from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -15,6 +20,8 @@ import {
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
 
 interface SignUpFirstStepProps {
    navigation: NativeStackNavigationProp<any, any>;
@@ -28,27 +35,49 @@ export function SignUpFirstStep({navigation}: SignUpFirstStepProps){
    }
 
    return (
-    <Container>
-       <Header>
-         <BackButton onPress={handleGoBack}/>
-         <Steps>
-            <Bullet active />
-            <Bullet />
-         </Steps>
-       </Header>
+      <KeyboardAvoidingView behavior='position' enabled >
+         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Container>
+               <Header>
+                  <BackButton onPress={handleGoBack}/>
+                  <Steps>
+                     <Bullet active />
+                     <Bullet />
+                  </Steps>
+               </Header>
 
-       <Title>
-         Crie sua{'\n'}conta
-       </Title>
-       <SubTitle>
-         Faça seu cadastro de{'\n'}
-         forma rápida e fácil
-       </SubTitle>
+               <Title>
+                  Crie sua{'\n'}conta
+               </Title>
+               <SubTitle>
+                  Faça seu cadastro de{'\n'}
+                  forma rápida e fácil
+               </SubTitle>
 
-       <Form>
-          <FormTitle>1. Dados</FormTitle>
-       </Form>
+               <Form>
+                  <FormTitle>1. Dados</FormTitle>
+                  <Input 
+                     iconName='user'
+                     placeholder='Nome'
+                  />
+                  <Input 
+                     iconName='mail'
+                     placeholder='E-mail'
+                  />
+                  <Input 
+                     iconName='credit-card'
+                     placeholder='CNH'
+                  />
 
-    </Container>
+               </Form>
+
+
+               <Button 
+                  title="Próximo"
+               />
+
+            </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
    );
 }
